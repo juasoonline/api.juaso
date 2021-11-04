66bf6d8aa3429eec90fbbe4fe7d8c5a366022949
Michael <billyston@gmail.com>
2021-11-04 11:43:30 +0000

    Task: Top Ranking Products (Home Page), Desc: Get some top ranked products grouped in categories, Status: Testing

--- app/Http/Resources/Juasoonline/Resource/Promotion/PromotionResource.php
+++ app/Http/Resources/Juasoonline/Resource/Promotion/PromotionResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Juasoonline\Resource\Promotion;
 
+use App\Http\Resources\Business\Resource\Product\Product\ProductResource;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -27,19 +28,12 @@ class PromotionResource extends JsonResource
 
                 'promo_start'               => Carbon::parse( $this -> resource -> promo_start ) -> diffForHumans(),
                 'promo_end'                 => Carbon::parse( $this -> resource -> promo_end ) -> diffForHumans(),
-                'status'                    => $this -> resource -> status,
-
-                'product_id'                => $this -> resource -> product -> resource_id,
-                'price'                     => number_format( $this -> resource -> product -> price, 2 ),
-                'sales_price'               => number_format( $this -> resource -> product -> sales_price, 2 ),
-                'total_sold'                => $this -> resource -> product -> total_sold,
-                'discount'                  => round(( $this -> resource -> product -> price - $this -> resource -> product -> sales_price ) / $this -> resource -> product -> price * 100, 2 ). '%',
-
-                'image'                     => $this -> resource -> product -> images[0] -> image,
 
                 'created_at'                => $this -> resource -> created_at -> toDateTimeString(),
                 'updated_at'                => $this -> resource -> updated_at -> toDateTimeString(),
-            ]
+            ],
+
+            'product'                       => new ProductResource( $this -> resource -> product ),
         ];
     }
 }
